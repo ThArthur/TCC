0db2bfeca87e31336aaa14b440e5f86c88db588c
Arthur Rocha Soares <arthurrocha993@gmail.com>
2022-05-26 13:39:49 -0300

    Init Projeto

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import Route from './src/route/route';
+
+export default function App(){
+  return(
+    <Route/>
+  );
+}
---
+++ src/pages/HomeAluno/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeAluno(){
+  return(
+    <View>
+        <Text>HomeAluno</Text>
+    </View>
+  );
+}
